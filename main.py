d4206db50d2ce179a71559ee9e2416cf9d3b40af
barbodGarsha <barbodgarshasbi79@gmail.com>
2018-12-11 15:34:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import neural_network_generator as aptnet
 import activation_functions
 
 # This is a test for XOR
+# Right now it just supports nueral networks with one output 
 a = aptnet.NeuralNet(2, [2], 1)
 a.set_activation_f(activation_functions.SIGMOID)
 samples_input = [[0, 0],
